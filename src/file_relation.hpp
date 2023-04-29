3a4a6fa20e01fb6db09c7756a08f3e03e2d77190
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-04-29 18:04:56 -0500

    feat(FileRelation/Iterator): Implement

--- src/file_relation.hpp
+++ src/file_relation.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <experimental/filesystem>
 #include <filesystem>
+#include <fstream>
+#include <iterator>
 #include <limits>
 #include <map>
+#include <optional>
 #include <string>
 
 #include "attribute.hpp"
@@ -14,6 +18,43 @@
 
 struct FileRelation : public Relation
 {
+    struct Iterator
+    {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using reference = nlohmann::json const&;
+        using pointer = nlohmann::json const*;
+
+    private:
+        std::string const m_filename;
+        mutable std::ifstream m_file;
+        uint64_t m_file_offset;
+        FileRelation const* m_fr;
+        mutable std::optional<nlohmann::json> m_value_read;
+        bool m_end = false;
+
+    public:
+        Iterator(std::string const& filename, uint64_t file_offset, FileRelation const* fr);
+        Iterator(Iterator const& it);
+
+        ~Iterator() = default;
+
+        auto operator*() const -> reference;
+        auto operator->() const -> pointer;
+
+        auto operator++() -> Iterator&;
+        auto operator++(int) -> Iterator;
+        auto operator--() -> Iterator&;
+        auto operator--(int) -> Iterator;
+
+        friend auto operator==(Iterator const& a, Iterator const& b) -> bool;
+        friend auto operator!=(Iterator const& a, Iterator const& b) -> bool;
+
+    private:
+        void advance_until_next_valid();
+    };
+
     constexpr static uint64_t const record_not_deleted = std::numeric_limits<uint64_t>::max();
     constexpr static uint64_t const record_deleted_and_last =
         std::numeric_limits<uint64_t>::max() - 1;
